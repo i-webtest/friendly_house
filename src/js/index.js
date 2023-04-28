4ccbee8364c4c3978f8710dafed181f1f35d79a0
i-webtest <stasbass80@gmail.com>
2023-04-28 18:12:41 +0300

    Added gulp

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import './plugins/swiper-bundle.min.js';
+import { burgerMenu } from './module/burgerMenu.js';
+import './module/swiper.js';
+// import Grid from './plugins/grid.js';
+
+burgerMenu();
+// Grid();
